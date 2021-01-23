db31085bf6e5ff2356c60abcb9d024f5cf239bfa
Vitor Barbosa <vitorbarbosa1998@hotmail.com>
2021-01-23 16:24:57 +0000

    fix: foix to deploy example

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,8 @@ export default function Home({ data, currentDate, github }) {
 }
 
 export async function getStaticProps() {
-  const res = await axios.get("http://localhost:3000/api/hello");
-  const json = res.data;
+  //const res = await axios.get("http://localhost:3000/api/hello");
+  const json = {}//res.data;
   const currentDate = new Date();
   let responseGitHub;
   try {
@@ -41,5 +41,27 @@ export async function getStaticProps() {
   }
 }
 
+/*
+Error.getInitialProps = ({ res, req, err }): Props => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+
+  if (statusCode === 404) {
+    if (req.url.match(/\/$/)) {
+      const withoutTrailingSlash = req.url.substr(0, req.url.length - 1);
+      if (res) {
+        res.writeHead(303, {
+          Location: withoutTrailingSlash
+        });
+        res.end();
+      }
+      else {
+        Router.push(withoutTrailingSlash);
+      }
+    }
+  }
+
+  return { statusCode };
+};
+*/
 
 
